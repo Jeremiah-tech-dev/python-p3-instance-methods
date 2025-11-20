b8adec187a2a814ef1d12442035af7b977525a0b
Jeremiah-tech-dev <jeremiah.koske@student.moringaschool.com>
2025-11-20 17:53:25 +0300

    Did the instances python lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,9 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    #Instance method definition
+    def walk(self):
+        print("The person is walking.")
